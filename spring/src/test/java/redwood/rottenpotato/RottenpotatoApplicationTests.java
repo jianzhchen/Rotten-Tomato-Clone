e4e53904c374f1f8ea87274f0ccca44e9f0b44f7
jianzhuangchen <jianzhuangchen@gmail.com>
2018-03-24 14:14:30 -0400

    spring gradle

---
+++ spring/src/test/java/redwood/rottenpotato/RottenpotatoApplicationTests.java
@@ -0,0 +1,16 @@
+package redwood.rottenpotato;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RottenpotatoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
