5d82882d482afa47ddd7ead912fe1b47b1a95000
jianzhuangchen <jianzhuangchen@gmail.com>
2018-04-08 19:56:01 -0400

    clean

---
+++ rottenpotato/src/main/java/com/redwood/rottenpotato/RottenpotatoApplication.java
@@ -0,0 +1,12 @@
+package com.redwood.rottenpotato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RottenpotatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RottenpotatoApplication.class, args);
+	}
+}
---
+++ rottenpotato/src/main/java/com/redwood/rottenpotato/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.redwood.rottenpotato.controllers;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    String home() {
+        return "Hello World!";
+    }
+
+}
