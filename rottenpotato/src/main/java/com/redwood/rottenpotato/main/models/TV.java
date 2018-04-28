ce791b89d80bfd10a832c699614ac6f6ef35e98d
LAPTOP-HR85DR53\chenj <jia.chen.1@stonybrook.edu>
2018-04-28 15:05:36 -0400

    Added TV model

---
+++ rottenpotato/src/main/java/com/redwood/rottenpotato/main/models/TV.java
@@ -0,0 +1,23 @@
+package com.redwood.rottenpotato.main.models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class TV {
+    //Primary key for the entity Item
+    @Id
+    private String TVKey;
+    private String TVName;
+    private String TVInfo;
+    private String TVLength;
+    private String TVGenre;
+    private String TVTime;
+    private String TVWriter;
+    private String TVCast;
+
+    //Constructor for JPA
+    protected TV() {
+    }
+
+}
