70eb2ab2e77785c3d1e96b1e584f3c98bb3e918b
xy <xy@qq>
2018-05-02 18:11:09 -0400

    some change

--- rottenpotato/src/main/resources/static/js/index.js
+++ rottenpotato/src/main/resources/static/js/index.js
@@ -1,11 +1,8 @@
 function getMovie(movieId){
-    $.get("/m/"+movieId,function (returndate) {
-        console.log(returndate)
-    })
+    window.location.href="/m/"+movieId;
 }
 
 function getTv(TvId){
-    $.get("/m/"+TvId,function (returndate) {
-        console.log(returndate)
-    })
+    window.location.href="/m/"+movieId;
+
 }
