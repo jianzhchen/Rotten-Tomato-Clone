3fa7223072b5bedf361937b6b86027e958aafca1
xy <xy@qq>
2018-05-12 01:55:10 -0400

    unfollow modal

--- rottenpotato/src/main/resources/static/js/profilePage.js
+++ rottenpotato/src/main/resources/static/js/profilePage.js
@@ -39,41 +39,6 @@ function deleteFromNotInterested(itemKey)
         "json");
 }
 
-
-function toggleFollowUnFollow(userId)
-{
-
-    var change = document.getElementById("toggleFollowUnFollow");
-    if (change.innerText === "FOLLOW")
-    {
-        $.post("/1/follow",
-            {"userId":userId},
-                function (message)
-                {
-                    location.reload();
-                },
-                "json");
-        change.innerText = "UNFOLLOW";
-        change.classList.remove("btn-success")
-        change.classList.add("btn-danger");
-    }
-    else
-    {
-        if (confirm("You Really Want To UNFOLLOW?"))
-        {
-            $.post("/1/unFollow",
-                {"userId":userId},
-                    function (message)
-                    {
-                        location.reload();
-                    },
-                    "json");
-            change.innerText = "FOLLOW";
-            change.classList.remove("btn-danger")
-            change.classList.add("btn-success");
-        }
-    }
-}
 function getItem(url) {
     window.location.href=url;
 }
---
+++ rottenpotato/src/main/resources/static/js/user.js
@@ -0,0 +1,40 @@
+function toggleFollowUnFollow(userId)
+{
+
+    var change = document.getElementById("toggleFollowUnFollow");
+    if (change.innerText === "FOLLOW")
+    {
+        $.post("/1/follow",
+            {"userId":userId},
+            function (message)
+            {
+                location.reload();
+            },
+            "json");
+        change.innerText = "UNFOLLOW";
+        change.classList.remove("btn-success")
+        change.classList.add("btn-danger");
+    }
+    else
+    {
+        $('#confirmModalHeader').html('Confirm');
+        $('#confirmModalBody').html('You Really Want To UNFOLLOW?');
+        $('#confirmModal').modal('show');
+        $('#confirmModalConfirmButton').click(function () {
+            $.post("/1/unFollow",
+                {"userId":userId},
+                function (message)
+                {
+                    location.reload();
+                },
+                "json");
+            change.innerText = "FOLLOW";
+            change.classList.remove("btn-danger");
+            change.classList.add("btn-success");
+        });
+
+
+
+
+    }
+}
