2ca80d4103a543ac75c382a2e809976410793840
jianzhuangchen <jianzhuangchen@gmail.com>
2018-05-12 16:13:35 -0400

    top critic

--- rottenpotato/src/main/java/com/redwood/rottenpotato/main/repositories/CriticReviewRepository.java
+++ rottenpotato/src/main/java/com/redwood/rottenpotato/main/repositories/CriticReviewRepository.java
@@ -5,6 +5,7 @@ import com.redwood.rottenpotato.main.models.CriticReview;
 import com.redwood.rottenpotato.main.models.Movie;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +13,11 @@ import java.util.List;
 @Repository
 public interface CriticReviewRepository extends JpaRepository<CriticReview, Long> {
     List<CriticReview> findByItemKey(String itemKey);
+
     List<CriticReview> findTop10ByOrderByReviewTimeDateDesc(Pageable pageable);
-    List<CriticReview> findTop10ByCriticKeyOrderByReviewTimeDateDesc(String criticKey,Pageable pageable);
+
+    List<CriticReview> findTop10ByCriticKeyOrderByReviewTimeDateDesc(String criticKey, Pageable pageable);
+
+    @Query("select c,sum(c.criticKey)as num from CriticReview c group by c.criticKey order by num desc ")
+    List<CriticReview> findTop10ByReviewCount();
 }
