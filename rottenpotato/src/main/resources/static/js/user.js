1c7ea5d48a6ac795777ef3580abe3d1cd9c82a6f
LAPTOP-HR85DR53\chenj <jia.chen.1@stonybrook.edu>
2018-05-12 17:04:18 -0400

    Update report functionality in user.html

--- rottenpotato/src/main/resources/static/js/user.js
+++ rottenpotato/src/main/resources/static/js/user.js
@@ -38,3 +38,18 @@ function toggleFollowUnFollow(userId)
 function getUser(key) {
     window.location.href="/u/"+key;
 }
+
+function reportReview(reviewId) {
+    $('#reportModal').modal('show');
+    $('#reportModalConfirmButton').click(function () {
+        var reason = $('#reportReason').val();
+        $.post("/1/reportReview",
+            {"reviewId":reviewId, "content": reason},
+            function (message) {
+                $('#generalModalHeader').html('Success');
+                $('#generalModalBody').html('You have send the report successfully!');
+                $('#generalModal').modal('show');
+            },
+            "json");
+    });
+}
